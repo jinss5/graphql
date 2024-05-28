a9922651731464732f89f445b993233375ca335a
jinss5 <jinss1145@gmail.com>
2024-05-27 18:00:12 -0700

    initialize

---
+++ server.js
@@ -0,0 +1,26 @@
+import { ApolloServer, gql } from "apollo-server";
+
+// SDL(schema defenition language) -> explain the shape
+
+// ehat is in theq uery type is what user will call
+const typeDefs = gql`
+  type User {
+    id: ID
+    username: String
+  }
+  type Tweet {
+    id: ID
+    text: String
+    author: User
+  }
+  type Query {
+    allTweets: [Tweet]
+    tweet(id: ID): Tweet
+  }
+`;
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(({ url }) => {
+  console.log(`Running on ${url}`);
+});
